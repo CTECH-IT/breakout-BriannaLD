91a165b9e7e90902ae27b0f3a2cf2b017d4c272d
BriannaLD <7979102@isd535.org>
2021-09-13 09:42:00 -0500

    Added Paddle Movement and Restrictions

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,93 @@
-// Put your JavaScript here
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d")
+
+let x = canvas.width/2;
+let y = canvas.height-30;
+
+let dx = 2;
+let dy = -2;
+
+let ballRadius = 10;
+
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+function drawBall() {
+//draw the ball
+ctx.beginPath();
+ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+ctx.fillStyle = "#0095DD";
+ctx.fill();
+ctx.closePath;
+}
+
+function drawPaddle () {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+//clear the canvas 
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+drawBall();
+drawPaddle();
+
+//change the x and y values for the ball
+x += dx;
+y += dy;
+
+if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+}
+
+if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+}
+
+//paddle controls
+if(rightPressed) {
+    paddleX += 7
+    if (paddleX + paddleWidth > canvas.width) {
+        paddleX = canvas.width - paddleWidth;
+    }
+}
+else if (leftPressed) {
+    paddleX -= 7;
+    if (paddleX < 0) {
+        paddleX = 0;
+    }
+}
+
+}
+
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+setInterval(draw, 10);
+
