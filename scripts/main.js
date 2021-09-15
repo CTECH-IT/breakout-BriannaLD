38b060f00d81e6cfd381a749bc4a7ca6bfbc9a84
BriannaLD <7979102@isd535.org>
2021-09-15 09:43:39 -0500

    Added blocks, working on collision detection.

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,23 @@ let paddleX = (canvas.width-paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+//set up a 2-dimensional array for the bricks
+let bricks = [];
+for (let c=0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r=0; r < brickRowCount; r++) {
+        bricks[c][r] = {x: 0, y: 0, show: true};
+    }
+}
+
 function drawBall() {
 //draw the ball
 ctx.beginPath();
@@ -33,12 +50,32 @@ function drawPaddle () {
     ctx.closePath();
 }
 
+function drawBricks () {
+    for (let c=0; c<brickColumnCount; c++){
+        for (let r=0; r<brickRowCount; r++){
+            if (bricks[c][r].show == true){
+            let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+            }
+        }
+    }
+}
+
 function draw() {
 //clear the canvas 
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+drawBricks();
 drawBall();
 drawPaddle();
+collisionDetection();
 
 //change the x and y values for the ball
 x += dx;
@@ -51,10 +88,15 @@ if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
 
 if (y + dy < ballRadius) { //ceiling check
     dy = -dy;
-} else if (y + dy > canvas.height - ballRadius) {
+} else if (y + dy > canvas.height - ballRadius) { //floor check
+
+    if (x > paddleX && x < paddleX + paddleWidth) { //paddle check
+        dy = -dy;
+    } else { //hit the floor
     alert ("GAME OVER - GET GOOD");
     document.location.reload();
     clearInterval(interval); //needed for browser to end the game
+    }
 }
 
 //paddle controls
@@ -71,6 +113,20 @@ else if (leftPressed) {
     }
 }
 
+}
+
+function collisionDetection() {
+    for (let c=0; c < brickColumnCount; c++){
+        for (let r=0; r < brickRowCount; r++){
+            let b = bricks[c][r];
+            if (b.show == true) {
+            if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+                dy = -dy;
+                b.show == false;
+                }
+            }
+        }
+    }
 }
 
 function keyDownHandler(e) {
